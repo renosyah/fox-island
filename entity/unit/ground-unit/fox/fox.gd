8fc127d7be8d3b758f85a4ad4adba3b28afe05cd
renosyah <syahputrareno975@gmail.com>
2022-10-12 10:37:36 +0700

    if fox fall below map, delete

--- entity/unit/ground-unit/fox/fox.gd
+++ entity/unit/ground-unit/fox/fox.gd
@@ -165,12 +165,20 @@ func _walk():
 func master_moving(delta):
 	.master_moving(delta)
 	
+	# fall below map
+	if translation.y < -25.0:
+		emit_signal("on_dead", self, hit_by_player)
+		set_physics_process(false)
+		queue_free()
+		return
+		
 	if _is_jump and is_on_floor():
 		_is_jump = false
 		rpc_unreliable("_land")
 		
 	if _is_roll and is_on_floor():
 		_is_roll = false
+		
 		
 func _on_animation_checker_timeout():
 	if is_dead:
